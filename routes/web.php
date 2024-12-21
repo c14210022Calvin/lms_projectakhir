ad12a519ca71423e9f8d9940b97aa076dd04e9b9
c14210022Calvin <c14210022@john.petra.ac.id>
2024-12-22 02:25:45 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware(['auth', 'verified', RoleMiddleware::class . ':admin'])->group
     Route::post('/books', [BookController::class, 'store'])->name('books.store');
 
     // Route Edit dan Update Buku
+    
     Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
     Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
     Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy'); // Tambahkan route ini
